0264a38295112dcb7edb64c28e33648c69284819
alex schuessler <schuessler.alex@gmail.com>
2022-10-21 16:19:22 -0500

    Example 2 solution finished.

---
+++ ex2.js
@@ -0,0 +1,31 @@
+let input1 = "The quick brown fox jumped over the lazy dog";
+let input2 = "Hello World";
+let input3 = "This is my third test case";
+
+
+let exampleTwo = (inputString) =>{
+    let outputString = "";
+    let repeatedCharacters= "";
+    for(let i = 0; i < inputString.length; i++){
+        if(outputString.includes(inputString[i])){
+            repeatedCharacters = repeatedCharacters.concat(inputString[i])
+            
+        }else{
+            outputString = outputString.concat(inputString[i])
+        };
+    }
+    console.log(inputString);
+    console.log("New String: " + outputString);
+    console.log("Repeated characters: " + repeatedCharacters)
+    console.log("")
+};
+
+// Time complexity: O(n), space: O(n). 
+// Need to iterate over entire input so n time
+// outputSting + repeatedCharacters = n, so n space
+
+
+exampleTwo(input1);
+exampleTwo(input2);
+exampleTwo(input3);
+
