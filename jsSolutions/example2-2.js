9f68112065199937a7063dbd3f9e2303c569905b
alex schuessler <schuessler.alex@gmail.com>
2022-10-22 09:51:50 -0500

    Finished with second example 2 solution.

--- jsSolutions/example2-2.js
+++ jsSolutions/example2-2.js
@@ -6,24 +6,31 @@ let input3 = "This is my third test case";
 let exampleTwo = (inputString) =>{
     let outputString = "";
     let duplciateCharacters= "";
+    let characterCount = {};
     for(let i = 0; i < inputString.length; i++){
-        if(outputString.includes(inputString[i])){
-            duplciateCharacters = duplciateCharacters.concat(inputString[i])
+        if(characterCount[inputString[i]] === undefined){
+            characterCount[inputString[i]] = 1;
+
+            duplciateCharacters = duplciateCharacters.concat(inputString[i]);
             
         }else{
-            outputString = outputString.concat(inputString[i])
+            outputString = outputString.concat(inputString[i]);
+            characterCount[inputString[i]]++;
         };
     }
     console.log("Input string: " + inputString);
     console.log("Original characters: " + outputString);
     console.log("Duplciate characters: " + duplciateCharacters)
-    console.log("New String: " + outputString + duplciateCharacters);
+    console.log("New String: " + duplciateCharacters + outputString);
+    // console.log(characterCount);
     console.log("");
 };
 
 // Time complexity: O(n), space: O(n). 
 // Need to iterate over entire input so n time
-// outputSting + repeatedCharacters = n, so n space
+// This solution I changed the if condition
+// Should be slightly faster because checking a key is O(1) vs O(n) for includes()
+// Still O(n) time and space
 
 
 exampleTwo(input1);
