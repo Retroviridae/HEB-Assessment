2e6668572b9928f45aec67288244cc93c93bce75
alex schuessler <schuessler.alex@gmail.com>
2022-10-22 11:06:14 -0500

    Got puppeteer working.

---
+++ jsSolutions/exercise1Solutions/solutions1-1.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+async function crawlHEB(url){
+    const browser = await puppeteer.launch({
+        args: [
+             '--disable-gpu',
+             '--disable-dev-shm-usage',
+             '--disable-setuid-sandbox',
+             '--no-first-run',
+             '--no-sandbox',
+             '--no-zygote',
+             '--single-process',
+        ]
+   })
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const [el] = await page.$x('//*[@id="root"]/header/div[2]/div/nav/ul/li[1]/a');
+    const href = await el.getProperty('href');
+    const hrefText = await href.jsonValue();
+
+    console.log({hrefText});
+
+    browser.close
+
+}
+
+crawlHEB("https://www.heb.com/")
+
